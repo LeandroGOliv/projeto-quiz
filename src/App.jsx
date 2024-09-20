4ea6b294485f99b4a542b092e0cc3c72902af884
Leandro Golub de Oliveira <160798808+LeandroGOliv@users.noreply.github.com>
2024-09-19 21:25:52 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+import Welcome from "./components/Welcome";
+// https://github.com/matheusbattisti/quiz_react/blob/main/src/img/welldone.svg
+
+function App() {
+  return (
+    <div>
+      <h1>Quiz de Programação</h1>
+      <Welcome />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Welcome.jsx
@@ -0,0 +1,15 @@
+import styles from "./Welcome.module.css";
+import quiz from "../img/quiz.svg";
+
+function Welcome() {
+  return (
+    <div>
+      <h2>Seja bem-vindo</h2>
+      <p>Clique no botão abaixo para começar:</p>
+      <button>Iniciar</button>
+      <img src={quiz} alt="Image quiz" />
+    </div>
+  );
+}
+
+export default Welcome;
