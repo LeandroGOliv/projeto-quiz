85dec9f0480757ac7a34c5a6e2e19f89d85295a8
Leandro Golub de Oliveira <160798808+LeandroGOliv@users.noreply.github.com>
2024-09-19 22:20:52 -0300

    Welcome component e estruturacao do projeto

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -3,7 +3,7 @@ import quiz from "../img/quiz.svg";
 
 function Welcome() {
   return (
-    <div>
+    <div className={styles.welcome}>
       <h2>Seja bem-vindo</h2>
       <p>Clique no botão abaixo para começar:</p>
       <button>Iniciar</button>
